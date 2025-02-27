9587f377730b827dcb520b7cb4480d6906bc965f
Tom Milewski <me@tm.codes>
2025-02-27 12:57:05 -0500

    feat: Pinned header version (#17)
    * feat: Pinned header version
    
    * test: Add tests for appending Clerk-API-Version
    
    * chore: Remove extra spaces

---
+++ src/Hooks/ClerkBeforeRequestHooks.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clerk\Backend\Hooks;
+
+use Psr\Http\Message\RequestInterface;
+
+class ClerkBeforeRequestHooks implements BeforeRequestHook 
+{
+    public function beforeRequest(BeforeRequestContext $context, RequestInterface $request): RequestInterface
+    {
+        return $request->withHeader(
+            'Clerk-API-Version',
+            '2024-10-01'
+        );
+    }
+}
--- src/Hooks/HookRegistration.php
+++ src/Hooks/HookRegistration.php
@@ -18,6 +18,8 @@ class HookRegistration
      */
     public static function initHooks(Hooks $hooks): void
     {
+        $hooks->registerBeforeRequestHook(new ClerkBeforeRequestHooks());
+        
         // $myHook = new MyHook();
 
         // $hooks->registerSDKInitHook($myHook);
